7dd0347a12d794cb1f3ac65ef949a78e2bcd69b6
MyYuan <1315626735@qq.com>
2021-11-19 01:20:32 +0800

    optimize network_up scripts

--- scripts/network_up.sh
+++ scripts/network_up.sh
@@ -16,11 +16,11 @@
 # Script to start paddleDTX and its dependent services with docker-compose.
 # Usage: ./network_up.sh {start|stop|restart}
 
-# PaddleDTX service contract name
+# PaddleDTX contract name
 # PaddleDTX服务启动需要安装的智能合约名称
 contractName=paddlempc
 contractAccount=1111111111111112
-# User's blockchain account address, used invoke contract 
+# User's blockchain account address, used invoke contract
 # 用户安装合约所使用的区块链账户地址
 addressPath=../testdata/blockchain/user
 transferAmount=110009887797
@@ -35,48 +35,47 @@ executor2PrivateKey=858843291fe4ed4bd2afc1120efd7315f3cae2d3f79e582f7df843ac6eb0
 namespaces=paddlempc
 
 function start() {
-  # 1. Standardized service config.toml
+  # 1. Standardize Conf
   # 1. 标准化配置文件
-  standardizedConf
+  standardizeConf
 
-  # 2. Start xchain
+  # 2. Start xchain network
   # 2. 启动区块链网络
   startXchain
 
-  # 3. Compile and install PaddleDTX contract
+  # 3. Compile and install paddleDTX contract
   # 3. 编译和安装智能合约
   installContract
   sleep 5
 
-  # 4. Start decentralized storage service
+  # 4. Start decentralized storage network
   # 4. 启动去中心化存储网络
-  startXdata
+  startXdb
   sleep 5
 
-  # 5. Start PaddleDTX service
+  # 5. Start PaddleDTX
   # 5. 启动多方安全计算网络
   startPaddleDTX
 }
 
 function stop() {
-  # Stop PaddleDTX service
+  # Stop PaddleDTX
   # 停止多方安全计算网络
-  print_blue "==========> Stop PaddleDTX service ..."
+  print_blue "==========> Stop executor network ..."
   docker-compose -f ../testdata/executor/docker-compose.yml down
-  # Stop decentralized storage service
+  # Stop decentralized storage network
   # 停止去中心化存储网络
-  print_blue "==========> Stop Decentralized storage service ..."
-  docker-compose -f ../testdata/xdata/docker-compose.yml down
+  print_blue "==========> Stop decentralized storage network ..."
+  docker-compose -f ../testdata/xdb/docker-compose.yml down
   # Stop xchain network
   # 停止区块链网络
-  print_blue "==========> Stop Xchain network ..."
+  print_blue "==========> Stop xchain network ..."
   docker-compose -f ../testdata/blockchain/docker-compose.yml down
-  sleep 3
-  print_green "==========> PaddleDTX service stopped !"
+  print_green "==========> PaddleDTX stopped !"
 }
 
 
-function standardizedConf() {
+function standardizeConf() {
   # Generate standard config.toml file
   # 生成标准配置文件
   sampleConfigFiles=`find $(dirname ${PWD})/testdata -name "config.toml.sample"`
@@ -84,53 +83,48 @@ function standardizedConf() {
   for file in $sampleConfigFiles
   do
     conf=${file%.sample}
-    cp -f $file $conf
-    sed -i "s/MNEMONIC/$mnemonic/g" $conf
-    sed -i "s/CONTRACTNAME/$contractName/g" $conf
-    sed -i "s/CONTRACTACCOUNT/$contractAccount/g" $conf
-    sed -i "s/EXECUTOR1PRIVATEKEY/$executor1PrivateKey/g" $conf
-    sed -i "s/EXECUTOR2PRIVATEKEY/$executor2PrivateKey/g" $conf
-    sed -i "s/LEASE1PRIVATEKEY/$executor1PrivateKey/g" $conf
-    sed -i "s/LEASE2PRIVATEKEY/$executor2PrivateKey/g" $conf
-    sed -i "s/FILESNAMESPACES/$namespaces/g" $conf
+    eval "cat <<EOF
+$(< $file)
+EOF
+"  > $conf
   done
 }
 
 # startXchain start xchain network with docker compose
 # 通过docker-compose启动区块链网络
 function startXchain() {
-  print_blue "==========> Xchain start ..."
+  print_blue "==========> Xchain network start ..."
   docker-compose -f ../testdata/blockchain/docker-compose.yml up -d
   sleep 6
 
   xchainContainers="xchain1.node.com xchain2.node.com xchain3.node.com"
   checkContainerStatus "$xchainContainers" "Xchain network"
-  print_green "==========> Xchain network start success !"
+  print_green "==========> Xchain network starts successfully !"
 }
 
-# startXdata start Decentralized storage service with docker compose
+# startXdb start Decentralized storage network with docker compose
 # 通过docker-compose启动去中心化存储网络
-function startXdata() {
-  print_blue "==========> Decentralized storage service start ..."
-  docker-compose -f ../testdata/xdata/docker-compose.yml up -d
+function startXdb() {
+  print_blue "==========> Decentralized storage network start ..."
+  docker-compose -f ../testdata/xdb/docker-compose.yml up -d
   sleep 6
 
-  xdataContainers="dataowner1.node.com dataowner2.node.com storage1.node.com storage2.node.com storage3.node.com"
-  checkContainerStatus "$xdataContainers" "Decentralized storage service"
-  print_green "==========> Decentralized storage service start success !"
+  xdbContainers="dataowner1.node.com dataowner2.node.com storage1.node.com storage2.node.com storage3.node.com"
+  checkContainerStatus "$xdbContainers" "Decentralized storage network"
+  print_green "==========> Decentralized storage network starts successfully !"
 }
 
-# startPaddleDTX start paddleDTX service with docker compose
+# startPaddleDTX start paddleDTX with docker compose
 # 通过docker-compose启动多网安全计算网络
 function startPaddleDTX() {
-  print_blue "==========> PaddleDTX service start ..."
+  print_blue "==========> Executor network start ..."
   docker-compose -f ../testdata/executor/docker-compose.yml up -d
   sleep 6
 
   executorContainers="executor1.node.com executor2.node.com"
   checkContainerStatus "$executorContainers" "PaddleDTX"
   print_green "========================================================="
-  print_green "          PaddleDTX service start success !            "
+  print_green "          PaddleDTX starts successfully !                "
   print_green "========================================================="
 }
 
@@ -157,15 +151,15 @@ function compileContract() {
       -e GOPROXY=https://goproxy.baidu-int.com \
       -e GO111MODULE=on \
       golang:1.13.4 sh -c "cd dai && go build -o ../testdata/blockchain/contract/$contractName ./blockchain/xchain/contract"
-  
-  # Copy contract file to xchain service container
+
+  # Copy contract file to xchain1 container
   # 将本地合约编译结果拷贝到区块链节点1容器中
   docker cp ../testdata/blockchain/contract/$contractName xchain1.node.com:/home/work/xchain/$contractName
   rm -f ../testdata/blockchain/contract/$contractName
 }
 
 function installContract() {
-  print_blue "==========> InstallContract start ..."
+  print_blue "==========> Install $contractName contract start ..."
   compileContract
 
   address=`cat $addressPath/address`
@@ -174,14 +168,14 @@ function installContract() {
   # Transfer token from miner's account to user's account
   # 从矿工账户给用户账户转移token
   transferAddressResult=`docker exec -it xchain1.node.com sh -c "
-    ./xchain-cli transfer --to $address --amount $transferAmount --keys ./data/keys --host xchain1.node.com:37101 
+    ./xchain-cli transfer --to $address --amount $transferAmount --keys ./data/keys --host xchain1.node.com:37101
   "`
   checkOperateResult "$transferAddressResult"
   # Get required fee to create the contract account
   # 获取创建合约账户所需要的fee
   accountFee=`docker exec -it xchain1.node.com sh -c "
     ./xchain-cli account new --account $contractAccount --host xchain1.node.com:37101 --keys ./user
-  " | grep "The gas you cousume is" | awk -F: '{print $2}'| sed -e 's/\r//g' | sed 's/ //g'`
+  " | grep "The gas you cousume is" | awk -F: '{print $2}' | awk 'BEGIN{RS="\r";ORS="";}{print $0}' | awk '$1=$1'`
 
   # Create xchain contract account
   # 创建合约账户
@@ -195,7 +189,7 @@ function installContract() {
   # Transfer token from miner's account to contract account
   # 从矿工账户给用户创建的合约账户转移token
   transferAccountResult=`docker exec -it xchain1.node.com sh -c "
-    ./xchain-cli transfer --to XC${contractAccount}@xuper --amount $transferAmount --keys ./data/keys  --host xchain1.node.com:37101 
+    ./xchain-cli transfer --to XC${contractAccount}@xuper --amount $transferAmount --keys ./data/keys  --host xchain1.node.com:37101
   "`
   checkOperateResult "$transferAccountResult"
 
@@ -208,7 +202,7 @@ function installContract() {
   # 获取安装合约所需要的fee
   contractFee=`docker exec -it xchain1.node.com sh -c "
     ./xchain-cli native deploy --account XC${contractAccount}@xuper --host xchain1.node.com:37101 --runtime go -a '{\"creator\":\"XC${contractAccount}@xuper\"}' --cname $contractName ./$contractName --keys ./user
-  " | grep "The gas you cousume is" | awk -F: '{print $2}' | sed -e 's/\r//g' | sed 's/ //g'`
+  " | grep "The gas you cousume is" | awk -F: '{print $2}' | awk 'BEGIN{RS="\r";ORS="";}{print $0}' | awk '$1=$1'`
 
   # Install contract
   # 安装智能合约
@@ -216,13 +210,13 @@ function installContract() {
     ./xchain-cli native deploy --account XC${contractAccount}@xuper --fee $contractFee  --host xchain1.node.com:37101 --runtime go -a '{\"creator\":\"XC${contractAccount}@xuper\"}' --cname $contractName ./$contractName --keys ./user
   "`
   checkOperateResult "$installResult"
-  print_green "==========> Install contract success ! "
+  print_green "==========> Install $contractName contract successfully ! "
 }
 
 function checkOperateResult() {
   errMessage=`echo "$1" | grep "Error\|Fail\|err"`
   if [ "$errMessage" ]; then
-    print_red "start network error: $1"
+    print_red "start paddleDTX error: $1"
     exit 1
   fi
 }
